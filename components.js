9c3c4babaf6d4f338b35a0aa051b37c9fccb2198
Anton Shishenya <anton.shishenya@gmail.com>
2018-02-14 17:38:37 +0300

    Improve logging and modify services to accept env variables as config

--- components.js
+++ components.js
@@ -3,16 +3,31 @@
 const option = require("commons/option");
 const search = require("commons/search");
 const bunyan = require("bunyan");
+require("dotenv").config();
 
 function init() {
     return option().config.then(config => {
-        module.exports.config = config;
-        module.exports.clientData = require("commons/client-data")(config["client-data"]);
-        module.exports.searcher = new search.Searcher(config.elastic);
-        module.exports.logger = bunyan.createLogger({
+        for (let key in config) {
+            if (process.env[key]) {
+                try {
+                    config[key] = JSON.parse(process.env[key]);
+                } catch (e) {
+                    config[key] = process.env[key];
+                }
+            }
+        }
+
+        const log = bunyan.createLogger({
             name: "client-api",
             level: config.log_level
         });
+
+        log.info({config});
+
+        module.exports.config = config;
+        module.exports.clientData = require("commons/client-data")(config["client-data"]);
+        module.exports.searcher = new search.Searcher(config.elastic);
+        module.exports.logger = log;
         return module.exports;
     })
     .catch(err => {
